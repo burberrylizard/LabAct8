5d8636f763a68784e4edf5a5a7dfe51e9e31eafe
chrlzjanem <charlizejanem0310@gmail.com>
2025-02-20 22:48:49 +0800

    Update testconn.php

--- testconn.php
+++ testconn.php
@@ -11,6 +11,7 @@ $conn = new mysqli($host, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
     echo "Database connection successful!";
 }
 
